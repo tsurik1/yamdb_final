6558720436d09a15e7ff3fc8cb7db476a7cc6752
yauheni <tsurik_01@mail.ru>
2023-02-03 00:28:35 +0300

    test flake server

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -7,7 +7,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'h_dr_qukd)ra#jso)!ck&wvp4)#mou986^jl^=_rjl
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', 'web']
+ALLOWED_HOSTS = ['localhost', 'web', '51.250.80.122', 'yatubetsurik.ddns.net']
 
 AUTH_USER_MODEL = 'users.User'
 
