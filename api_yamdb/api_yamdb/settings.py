4db7aed41c3c1ea52dbee38fbb628d39ea8e2926
yauheni <tsurik_01@mail.ru>
2023-02-07 01:13:55 +0300

    test

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -110,13 +110,13 @@ USE_L10N = True
 
 USE_TZ = True
 
-STATIC_URL = '/data/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'data')
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-STATICFILES_DIRS = ['/app/data']
+STATICFILES_DIRS = ['/app/static']
 
 UNUSED_USERNAME = 'me'
 
--- api_yamdb/data/data/wtf.py
+++
@@ -1,7 +0,0 @@
-import csv
-
-with open('category.csv', newline='') as csvfile:
-    reader = csv.DictReader(csvfile)
-
-    for row in reader:
-        print(row['first_name'], row['last_name'])
