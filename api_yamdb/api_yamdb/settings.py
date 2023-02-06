b1157f2b7e1da6d44908596770b8c7acec0b5b0e
yauheni <tsurik_01@mail.ru>
2023-02-07 00:32:12 +0300

    test

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -110,8 +110,8 @@ USE_L10N = True
 
 USE_TZ = True
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/data/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'data')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
