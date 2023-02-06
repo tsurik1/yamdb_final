c66c82e3ad540107e3f70464fb1657ab6a4f37bd
yauheni <tsurik_01@mail.ru>
2023-02-07 00:24:24 +0300

    test

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -116,6 +116,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+STATICFILES_DIRS = ['/app/data']
+
 UNUSED_USERNAME = 'me'
 
 FROM = 'noreply@noreply.com'
