e151a8f691e47e403843c7aa4b4e692d4d32040b
yauheni <tsurik_01@mail.ru>
2023-02-08 18:59:05 +0300

    test 433

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -116,12 +116,12 @@ USE_TZ = True
 # else:
 #     STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-STATICFILES_DIRS = ['/app/static']
+STATICFILES_DIRS = ['/app/static/']
 
 UNUSED_USERNAME = 'me'
 
